2c1b13002301817cec60cd789a932fdf5b77ff41
Demo <demo@Demos-MacBook-Pro.local>
2020-01-15 14:56:13 -0500

    Added notification with a timer

--- app/src/main/java/com/example/flex_break/MainActivity.java
+++ app/src/main/java/com/example/flex_break/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.flex_break;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String CHANNEL_ID = "test Channel";
+    private final int NOTIFICATION_ID = 001;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,54 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        createNotificationChannel();
+    }
+
+    public void onNotify(View v) {
+        testTimer(5);
+    }
+
+    public void timerNotify() {
+        //Log.d("log", "Button works");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_flex_stretch_notification_icon);
+        builder.setContentTitle("Flex Stretch");
+        builder.setContentText("It's time to stretch!");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //CharSequence name = getString(R.string.channel_name);
+            //String description = getString(R.string.channel_description);
+            CharSequence name = "Test Channel";
+            String description = "Test channel description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void testTimer(int time) {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                timerNotify();
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(timerTask, time * 1000);
     }
 
 }
